92bcac63f0e0a5d3420b84aab1b5b9b5c79414e7
vijaymarka2020 <166855261+vijaymarka2020@users.noreply.github.com>
2024-04-16 14:37:59 +0530

    Update AddArticle.jsx with category
    Updated AddArticle.jsx with category

--- src/components/AddArticle.jsx
+++ src/components/AddArticle.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Timestamp, collection, addDoc } from "firebase/firestore";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { storage, db, auth } from "./../firebaseConfig";
+import { db, auth } from "./../firebaseConfig";
 import { toast } from "react-toastify";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
@@ -15,10 +14,10 @@ export default function AddArticle() {
     description: "",
     image: "",
     category: "",
+    newstype: "",
+    gallery: ["", "", ""], // Initialize gallery array with empty strings
     createdAt: Timestamp.now().toDate(),
   });
-
-  const [progress, setProgress] = useState(0);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -28,7 +27,13 @@ export default function AddArticle() {
     setFormData({ ...formData, image: e.target.files[0] });
   };
 
-    const handlePublish = () => {
+  const handleGalleryChange = (e, index) => {
+    const updatedGallery = [...formData.gallery]; // Create a copy of the gallery array
+    updatedGallery[index] = e.target.value; // Update the value at the specified index
+    setFormData({ ...formData, gallery: updatedGallery }); // Update the state with the new gallery array
+  };
+
+  const handlePublish = () => {
     if (!formData.name || !formData.description) {
       alert("Please fill all the required fields");
       return;
@@ -39,11 +44,12 @@ export default function AddArticle() {
       name: formData.name,
       description: formData.description,
       source: formData.source,
-      url: formData.url,
       image: formData.image,
+      url: formData.url,
       createdBy: user.displayName,
       userId: user.uid,
       category: formData.category,
+      gallery: formData.gallery,
       createdAt: Timestamp.now().toDate(),
       likes: [],
       comments: [],
@@ -53,10 +59,10 @@ export default function AddArticle() {
         setFormData({
           name: "",
           source: "",
+          image: "",
           url: "",
           description: "",
           category: "",
-          image: ""
         });
       })
       .catch((err) => {
@@ -64,12 +70,78 @@ export default function AddArticle() {
       });
   };
 
+  const renderNewsTypeForm = () => {
+    switch (formData.newstype) {
+      case "news":
+        return (
+          <>
+            {/* News specific fields */}
+            {/* Category */}
+            <label htmlFor="">Category</label>
+            <select
+              name="category"
+              className="form-control"
+              value={formData.category}
+              onChange={(e) => handleChange(e)}
+            >
+              <option value="">Select category</option>
+              <option value="movies">movies</option>
+              <option value="national">national</option>
+              <option value="international">international</option>
+              <option value="sports">sports</option>
+              <option value="state">state</option>
+            </select>
+
+            <label htmlFor="">Source</label>
+            <input
+              type="text"
+              name="source"
+              className="form-control"
+              value={formData.source}
+              onChange={(e) => handleChange(e)}
+            />
+            <label htmlFor="">Url</label>
+            <input
+              type="text"
+              name="url"
+              className="form-control"
+              value={formData.url}
+              onChange={(e) => handleChange(e)}
+            />
+            <label htmlFor="">Image Url</label>
+            <input
+              type="text"
+              name="image"
+              className="form-control"
+              value={formData.image}
+              onChange={(e) => handleChange(e)}
+            />
+          </>
+        );
+      case "image":
+        return (
+          <>
+            {/* Image specific fields */}
+            <label htmlFor="">Gallery</label>
+            {formData.gallery.map((url, index) => (
+              <input
+                key={index}
+                type="text"
+                name="gallery"
+                className="form-control"
+                value={url}
+                onChange={(e) => handleGalleryChange(e, index)}
+              />
+            ))}
+          </>
+        );
+      default:
+        return null;
+    }
+  };
 
   return (
-    <div
-      className="border p-3 mt-3 mb-5 bg-light"
-      // style={{ position: "fixed" }}
-    >
+    <div className="border p-3 mt-3 mb-5 bg-light">
       {!user ? (
         <>
           <h2>
@@ -80,6 +152,20 @@ export default function AddArticle() {
       ) : (
         <>
           <h2>Create article</h2>
+          <>
+            {/* News type */}
+            <label htmlFor="">News Type</label>
+            <select
+              name="newstype"
+              className="form-control"
+              value={formData.newstype}
+              onChange={(e) => handleChange(e)}
+            >
+              <option value="">Select News Type</option>
+              <option value="news">News</option>
+              <option value="image">Image</option>
+            </select>
+          </>
           <div className="form-group">
             <label htmlFor="">Name</label>
             <input
@@ -91,7 +177,7 @@ export default function AddArticle() {
             />
           </div>
 
-          {/* description */}
+          {/* Description */}
           <label htmlFor="">Description</label>
           <textarea
             name="description"
@@ -99,46 +185,10 @@ export default function AddArticle() {
             value={formData.description}
             onChange={(e) => handleChange(e)}
           />
-          <label htmlFor="">category</label>
-          <textarea
-            name="category"
-            className="form-control"
-            value={formData.category}
-            onChange={(e) => handleChange(e)}
-          />
-          <label htmlFor="">Source</label>
-          <textarea
-            name="source"
-            className="form-control"
-            value={formData.source}
-            onChange={(e) => handleChange(e)}
-          />
-          <label htmlFor="">Image</label>
-          <textarea
-            name="image"
-            className="form-control"
-            value={formData.image}
-            onChange={(e) => handleChange(e)}
-          />
-          <label htmlFor="">Url</label>
-          <textarea
-            name="url"
-            className="form-control"
-            value={formData.url}
-            onChange={(e) => handleChange(e)}
-          />
 
-        
-          {progress === 0 ? null : (
-            <div className="progress">
-              <div
-                className="progress-bar progress-bar-striped mt-2"
-                style={{ width: `${progress}%` }}
-              >
-                {`uploading image ${progress}%`}
-              </div>
-            </div>
-          )}
+          {/* Render category-specific form */}
+          {renderNewsTypeForm()}
+
           <button
             className="form-control btn-primary mt-2"
             onClick={handlePublish}
